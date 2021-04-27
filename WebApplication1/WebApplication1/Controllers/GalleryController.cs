dfd372659beed4f01940199be5c68d90a5d26917
Michał <KrzyzowskiM1998@gmail.com>
2021-04-27 13:59:45 +0200

    ff

---
+++ WebApplication1/WebApplication1/Controllers/GalleryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public async Task<IActionResult> Index(int page=1)
+        {
+            page = page < 1 ? 1 : page;
+            var client = new HttpClient();
+            var path = $"https://api.unsplash.com/photos?client_id=nviiZIrxgImnT3mQ9QDoSKAOuJ291HWqcaXsjv7u0Mo&page={page}";
+            
+            var response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseStr = await response.Content.ReadAsStringAsync();
+                var photosData= JsonConvert.DeserializeObject<PhotoData[]>(responseStr);
+
+                ViewBag.CurrentPage = page;
+
+                return View(photosData);
+            }
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/PhotoData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class PhotoData
+    {
+        public UrlsData Urls { get; set; }
+    }
+    public class UrlsData
+    {
+        public string Raw { get; set; }
+        public string Full { get; set; }
+        public string Regular{ get; set; }
+        public string Small { get; set; }
+        public string Thumb{ get; set; }
+    }
+}
